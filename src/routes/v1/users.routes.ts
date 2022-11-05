07dcb07437f183fbbfbbe76541abc9ff4efabc9d
copy-ninja1 <emmajnr1000@gmail.com>
2022-11-05 23:10:20 +0100

    fix: verify bank account number

--- src/routes/v1/users.routes.ts
+++ src/routes/v1/users.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import SessionController from '@controllers/session.controller.';
 import UserController from '@controllers/user.controller';
 import validate from '@middlewares/validate';
-import { createUserSchema, createSessionSchema } from '@schemas/index';
+import { createUserSchema, createSessionSchema, verifyBankAccountSchema } from '@schemas/index';
 
 const userRouter = Router();
 const userController = new UserController();
@@ -17,4 +17,9 @@ userRouter.get('/:id/groups', userController.getUserGroups);
 // userRouter.patch('/:userId', validate(groupAddUser), userController.addUserToGroup);
 userRouter.put('/:id', userController.updateOne);
 userRouter.delete('/:id', userController.delete);
+userRouter.post(
+  '/verify/bank-account',
+  validate(verifyBankAccountSchema),
+  userController.verifyAccountNumber
+);
 export default userRouter;
